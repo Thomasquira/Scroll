2c67d73f98aea1a3f1eb1b45283d0fa48c512df4
Thomasquira <thomasquirogudelo@hotmail.com>
2025-11-20 20:53:21 +0100

    Inicio

---
+++ app/src/main/java/com/ejemplo/task13task1_3/MainActivity.java
@@ -0,0 +1,52 @@
+package com.ejemplo.task13task1_3;
+
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText article;
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.article), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        article = findViewById(R.id.article);
+
+        article.setEnabled(false);
+
+       btn = findViewById(R.id.add_comment);
+    }
+
+    public void comentar(View view) {
+    if (!article.isEnabled()) {
+            article.setEnabled(true);
+            btn.setText("Save");
+        } else {
+            article.setEnabled(false);
+            btn.setText("Add Comment");
+        }
+    }
+}
